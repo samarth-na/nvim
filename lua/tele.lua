e66c96a52d9d97fbae277cf14b1a3974cb069526
samarth-nagar <samarthxma2009@gmail.com>
2024-06-05 21:57:52 +0530

    backup

--- lua/tele.lua
+++ lua/tele.lua
@@ -13,19 +13,6 @@ require('telescope').setup({
     },
 })
 
-local actions = require("telescope.actions")
-local trouble = require("trouble.providers.telescope")
-
-local telescope = require("telescope")
-
-telescope.setup {
-    defaults = {
-        mappings = {
-            i = { ["<c-t>"] = trouble.open_with_trouble },
-            n = { ["<c-t>"] = trouble.open_with_trouble },
-        },
-    },
-}
 Builtin = require 'telescope.builtin'
 
 vim.keymap.set('n', '<leader>fo', Builtin.oldfiles, { desc = 'old  opened files' })
