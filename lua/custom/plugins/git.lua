8f21a3d95fda995adbe6c2bd62ee9edd02d624ca
samarth-nagar <samarthxma2009@gmail.com>
2024-06-15 13:20:24 +0530

    backup

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -5,11 +5,12 @@ return {
         'lewis6991/gitsigns.nvim',
         opts = {
             signs = {
-                add = { text = '+' },
+                add = { text = '┆' },
                 change = { text = '~' },
                 delete = { text = '_' },
                 topdelete = { text = '‾' },
                 changedelete = { text = '~' },
+                normal = { text = '✎' },
             },
         },
     },
