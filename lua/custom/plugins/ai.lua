f0885651135156ecfe2605666015d8607ed69679
Samarth Nagar <samarthxma2009@gmail.com>
2024-10-09 23:21:01 +0530

    main changes in ai plugins

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -8,11 +8,10 @@ return {
                     clear_suggestion = "<C-x>",
                     accept_word = "<C-f>",
                 },
-                ignore_filetypes = { cpp = true },
+                ignore_filetypes = { cpp = true, copilotchat = true, markdown = true, },
                 color = {
                     suggestion_color = "#ffffff",
                 },
-                ignore_filetypes = { "TelescopePrompt", "cmp_menu", "copilot", "markdown" },
                 disable_inline_completion = false, -- disables inline completion for use with cmp
                 disable_keymaps = false            -- disables built in keymaps for more manual control
             })
--- lua/kickstart/plugins/copilot-chat.lua
+++ lua/kickstart/plugins/copilot-chat.lua
@@ -17,24 +17,24 @@ return {
 
       show_folds = true, -- Shows folds for sections in chat
       show_help = false, -- Shows help message as virtual lines when waiting for user input
-      auto_follow_cursor = true, -- Auto-follow cursor in chat
-      auto_insert_mode = true, -- Automatically enter insert mode when opening window and on new prompt
+      auto_follow_cursor = false, -- Auto-follow cursor in chat
+      auto_insert_mode = false, -- Automatically enter insert mode when opening window and on new prompt
       insert_at_end = false, -- Move cursor to end of buffer when inserting text
       clear_chat_on_new_prompt = false, -- Clears chat on every new prompt
       highlight_selection = true, -- Highlight selection in the source buffer when in the chat window
 
       window = {
-        layout = 'vertical',    -- 'vertical', 'horizontal', 'float', 'replace'
-        width = 0.4,            -- fractional width of parent, or absolute width in columns when > 1
-        height = 0.5,           -- fractional height of parent, or absolute height in rows when > 1
+        layout = 'vertical',  -- 'vertical', 'horizontal', 'float', 'replace'
+        width = 0.5,          -- fractional width of parent, or absolute width in columns when > 1
+        height = 0.5,         -- fractional height of parent, or absolute height in rows when > 1
         -- Options below only apply to floating windows
-        relative = 'editor',    -- 'editor', 'win', 'cursor', 'mouse'
-        border = 'single',      -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
-        row = nil,              -- row position of the window, default is centered
-        col = nil,              -- column position of the window, default is centered
-        title = 'Copilot Chat', -- title of chat window
-        footer = nil,           -- footer of chat window
-        zindex = 1,             -- determines if window is on top or below other floating windows
+        relative = 'editor',  -- 'editor', 'win', 'cursor', 'mouse'
+        border = 'single',    -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+        row = nil,            -- row position of the window, default is centered
+        col = nil,            -- column position of the window, default is centered
+        title = 'Copilot.md', -- title of chat window
+        footer = nil,         -- footer of chat window
+        zindex = 1,           -- determines if window is on top or below other floating windows
       },
     },
     -- See Commands section for default commands if you want to lazy load on them
