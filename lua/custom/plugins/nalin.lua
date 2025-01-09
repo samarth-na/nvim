adbbe8c6cc86253ba83e206ac8d2d17be4a4a9d2
samarth nagar <samarth07nagar@gmail.com>
2025-01-10 00:02:36 +0530

    main added spotify

--- lua/custom/plugins/nalin.lua
+++ lua/custom/plugins/nalin.lua
@@ -10,7 +10,22 @@ return {
 
         event = "VeryLazy"
 
-    }
+    },
+    {
+        "stsewd/spotify.nvim",
+        build = ":UpdateRemotePlugins",
+        config = function()
+            require("spotify").setup()
+        end,
+        init = function()
+            -- Optional mappings.
+            -- vim.keymap.set("n", "<leader>ss", ":Spotify play/pause<CR>", { silent = true })
+            -- vim.keymap.set("n", "<leader>sj", ":Spotify next<CR>", { silent = true })
+            -- vim.keymap.set("n", "<leader>sk", ":Spotify prev<CR>", { silent = true })
+            -- vim.keymap.set("n", "<leader>so", ":Spotify show<CR>", { silent = true })
+            -- vim.keymap.set("n", "<leader>sc", ":Spotify status<CR>", { silent = true })
+        end,
+    },
 }
 -- IMP:
 -- format to add plugins:
